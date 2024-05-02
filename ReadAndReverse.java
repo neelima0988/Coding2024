c9ed4cbc9cc1c9ca755926225e485fd1f07eb5f6
Neelima Chhotwani <neelima0988@gmail.com>
2024-05-02 12:21:09 +0530

    Update ReadAndReverse.java

--- ReadAndReverse.java
+++ ReadAndReverse.java
@@ -15,56 +15,20 @@ import java.util.stream.Stream;
 public class ReadAndReverse {
 
 	public static void main(String[] args) {
-		System.out.println("I am running");
-		ClassLoader loader = Thread.currentThread().getContextClassLoader();
-		InputStream  is = loader.getResourceAsStream("Content.txt");
-		 ArrayList<String> lines = new ArrayList<String>();
-		 
-		    InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.US_ASCII);
-		    
+		System.out.println("Welcome HSBC, Thank You for Considering my Candidature for the Role");
 		
-		try ( BufferedReader br = new BufferedReader(streamReader)) {
-		    String line;
-		    while ((line = br.readLine()) != null) {
-		    	System.out.println(line);
-		    	 StringBuilder input1 = new StringBuilder();
-		    	 
-		         // append a string into StringBuilder input1
-		         input1.append(line);
-		  
-		         // reverse StringBuilder input1
-		         input1.reverse();
-		    	lines.add(line);
-		       /*for (int pos = line.indexOf("\n"); pos != -1; pos = line.indexOf("\n", pos + 1)) {
-		        System.out.println("\\n at " + pos);;
-		       }*/
-		    }
-		    
-		    StringBuilder builder = new StringBuilder();
-		    builder.append("Hello World");
-		    
-	        // reverse StringBuilder input1
-		    builder.reverse();
-	 
-	        // print reversed String
-	        System.out.println(builder);
-		    builder.reverse();
-		    
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		
+		System.out.println("Welcome HSBC, Thank You for Considering my Candidature for the Role");
 		
 		try(PrintWriter out = 
-		        new PrintWriter(Files.newBufferedWriter(Paths.get("C:\\Users\\Sumit\\Neelima-JavaWS\\CITICodingTest\\src\\out.txt")))) {
-		    Files.lines(Paths.get("C:\\Users\\Sumit\\Neelima-JavaWS\\CITICodingTest\\src\\Content2.txt"))
+		        new PrintWriter(Files.newBufferedWriter(Paths.get("output.txt")))) {
+		    Files.lines(Paths.get("input.txt"))
 		    .flatMap(line -> Stream.of(new StringBuilder(line.toString()).reverse()))
 		          .collect(Collectors.toCollection(LinkedList::new))
-		         .descendingIterator()
+		         .iterator()
 		         .forEachRemaining(out::println);
 		}catch (Exception e) {
 
-e.printStackTrace();
+			System.out.println(" There was an eroor while reading or Writing File, PLease check exception for details"+e.getMessage());
 		}
 		
 		
